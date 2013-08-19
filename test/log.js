e969a60690228469462f2d18c792edd58575f154
Robin Harper <cabrel@zerklabs.com>
2013-08-19 17:07:53 -0400

    Fixed issue not applying additional format parameters

--- test/log.js
+++ test/log.js
@@ -8,6 +8,10 @@ describe('log', function() {
   describe('#print', function() {
     var _consoleOutput = [];
 
+    process.stdout.on('data', function(data) {
+      console.log(data);
+    });
+
     beforeEach(function() {
       console.log = function() { _consoleOutput.push(_consoleOutput.slice.call(arguments)) };
     });
@@ -20,6 +24,15 @@ describe('log', function() {
       stockpile.log.print('test');
       expect(_consoleOutput.length).to.be(1);
       expect(_consoleOutput[0][0]).to.contain('test');
+
+      console.log = originalConsole;
+    });
+
+    it('should apply additional format parameters', function() {
+      stockpile.log.print('%s: %s', 'test', 'test1');
+      expect(_consoleOutput.length).to.be(1);
+      expect(_consoleOutput[0][0]).to.contain('test:');
+      expect(_consoleOutput[0][0]).to.contain(' test1');
 
       console.log = originalConsole;
     });
