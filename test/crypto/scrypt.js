dcc87e846aa207f36b833186a2fe310cd0e992b5
Robin Harper <cabrel@zerklabs.com>
2013-11-19 10:40:42 -0500

    Added ability to change scrypt maxtime
    v1.4.2

--- test/crypto/scrypt.js
+++ test/crypto/scrypt.js
@@ -23,14 +23,29 @@ describe('scrypt', function() {
       });
     });
 
+    it('should return a string if given a password and maxtime (of 1)', function(done) {
+      scrypt.generate('password123', 1).done(function(result) {
+        expect(result).to.be.a('string');
+        done();
+      });
+    });
+
   });
 
   describe('#authenticate', function() {
     var validHash = 'c2NyeXB0ABEAAAAIAAAAAaGkTdCE1EBGvzIJCpvChUSaFVErcIjQduvVEp6PCEawreAtP9m69xUYmhTSTLF++DbiHXmdWYRik2VF2LxBnJ6nfmhGxNNp0YzPgaR39amA'; // password123 with maxtime = 3.0 (3 seconds)
     var invalidHash = 'abcc2NyeXB0ABEAAAAIAAAAAaGkTdCE1EBGvzIJCpvChUSaFVErcIjQduvVEp6PCEawreAtP9m69xUYmhTSTLF++DbiHXmdWYRik2VF2LxBnJ6nfmhGxNNp0YzPgaR39amA'; // added 'abc' to beginning of string
+    var testHash = 'c2NyeXB0AA8AAAAIAAAAAbM5Iwal+z/M0FIJsA4c8VZwyBwZVamI9LYqWfeiy83F2ATfeQGt7A3cao77RGOitiwv7sCj603xV3AMxifp9yeMyziCcnrHRPxT9S5SeNND';
 
     it('should return true if given valid password and hash', function(done) {
       scrypt.authenticate('password123', validHash).done(function(result) {
+        expect(result).to.be(true);
+        done();
+      });
+    });
+
+    it('should return true if given valid password and hash from a shorter maxtime (1) than the default (3.8)', function(done) {
+      scrypt.authenticate('password123', testHash).done(function(result) {
         expect(result).to.be(true);
         done();
       });
