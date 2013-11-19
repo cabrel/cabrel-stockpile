47ddfdff588feeb4a891f7fc0455e9a2a98b42ba
Robin Harper <cabrel@zerklabs.com>
2013-11-19 13:43:20 -0500

    v1.4.3

--- test/general/strings.js
+++ test/general/strings.js
@@ -20,4 +20,35 @@ describe('strings', function() {
       expect(fn).to.throwError();
     });
   });
+
+  describe('#parseNTUsername', function() {
+    it('should return an object containing domain and username (Netbios username)', function() {
+      var username = 'DOMAIN\\username';
+      var result = strings.parseNTUsername(username);
+
+      expect(result).to.have.property('domain');
+      expect(result.domain).to.be('DOMAIN');
+
+      expect(result).to.have.property('username');
+      expect(result.username).to.be('username');
+    });
+
+    it('should return an object containing domain and username (User Principal Name)', function() {
+      var username = 'username@domain.local';
+      var result = strings.parseNTUsername(username);
+
+      expect(result).to.have.property('domain');
+      expect(result.domain).to.be('domain');
+
+      expect(result).to.have.property('username');
+      expect(result.username).to.be('username');
+    });
+
+    it('should return null if given an unknown format', function() {
+      var username = 'username|domain.local';
+      var result = strings.parseNTUsername(username);
+
+      expect(result).to.be(null);
+    });
+  });
 });
