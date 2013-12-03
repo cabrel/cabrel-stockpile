51416e800d44d3fb98ce930ebdb8606e98c21832
Robin Harper <cabrel@zerklabs.com>
2013-12-03 16:41:45 -0500

    Fixed global leak
    v1.5.1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-module.exports = require('./lib/stockpile');
+// third-party exports
+exports.third = {
+  alphaID: require('./lib/third/alphaID'),
+  async: require('async'),
+  hapi: require('hapi'),
+  lodash: require('lodash'),
+  moment: require('moment'),
+  q: exports.Q = require('q')
+};
+
+// local exports
+exports.log = require('./lib/general/log');
+exports.RegExes = require('./lib/general/RegExes');
+exports.strings = require('./lib/general/strings');
+exports.checks = require('./lib/general/checks');
+
+// hapi helpers
+exports.hapi = {
+  errors: require('./lib/hapi/errors')
+};
+
+// conversion helpers
+exports.conversions = {
+  ldap: require('./lib/conversions/ldap'),
+  periods: require('./lib/conversions/periods')
+};
