159f891edfcaa6076eea1182806da4cd518d4df7
Robin Harper <cabrel@zerklabs.com>
2013-12-06 14:48:50 -0500

    Reorganized tests added 'internal' function to the errors wrapper to wrap Hapi.errors.internal
    v1.5.2

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
 // third-party exports
-exports.third = {
-  alphaID: require('./lib/third/alphaID'),
-  async: require('async'),
-  hapi: require('hapi'),
-  lodash: require('lodash'),
-  moment: require('moment'),
-  q: exports.Q = require('q')
-};
+exports.alphaID = require('./lib/third/alphaID');
 
 // local exports
 exports.log = require('./lib/general/log');
 exports.RegExes = require('./lib/general/RegExes');
 exports.strings = require('./lib/general/strings');
 exports.checks = require('./lib/general/checks');
+exports.request = require('./lib/general/request_wrapper');
 
 // hapi helpers
-exports.hapi = {
-  errors: require('./lib/hapi/errors')
-};
+exports.errors = require('./lib/hapi/errors');
 
 // conversion helpers
 exports.conversions = {
---
+++ test/hapi.js
@@ -0,0 +1,36 @@
+var expect = require('expect.js'),
+    stockpile = require('../'),
+    errors = stockpile.errors,
+    util = require('util');
+
+
+describe('Hapi', function() {
+  describe('errors', function() {
+    describe('#passThrough', function() {
+      it('should return a Hapi.errors.passThrough object', function(done) {
+        var passThrough = errors.passThrough(new Error('I am an error'));
+
+        expect(passThrough).to.have.key('isBoom');
+        expect(passThrough).to.have.key('response');
+        expect(passThrough).to.have.key('message');
+        expect(passThrough.message).to.be('Pass-through');
+
+        done();
+      });
+    });
+
+    describe('#internal', function() {
+      it('should return a Hapi.errors.internal object', function(done) {
+        var internal = errors.internal(new Error('I am an error'));
+
+        expect(internal).to.have.key('isBoom');
+        expect(internal).to.have.key('response');
+        expect(internal).to.have.key('message');
+        expect(internal).to.have.key('trace');
+        expect(internal).to.have.key('outterTrace');
+
+        done();
+      });
+    });
+  });
+});
