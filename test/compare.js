f9e39a3661aa2ec931c84d917d8bc2e5885016cd
Robin Harper <cabrel@zerklabs.com>
2013-12-10 00:23:04 -0500

    Fixed matchObjectId to use a filter when processing an array
    v1.5.4

--- test/compare.js
+++ test/compare.js
@@ -60,4 +60,88 @@ describe('compare', function() {
       expect(compare.ObjectID.test('0')).to.equal(false);
     });
   });
+
+  describe('#matchObjectId()', function() {
+    it('should find a match in an array', function() {
+      var source = [
+        '0', '1', '2', '3', '4'
+      ];
+      var toFind = '3';
+      expect(compare.matchObjectId(source, toFind)).to.equal(true);
+    });
+
+    it('should find return true if given a matching string source', function() {
+      var source = '3';
+      var toFind = '3';
+      expect(compare.matchObjectId(source, toFind)).to.equal(true);
+    });
+
+    it('should find return true if given an object with an _id property', function() {
+      var source = { _id: '3' };
+      var toFind = '3';
+      expect(compare.matchObjectId(source, toFind)).to.equal(true);
+    });
+
+    it('should find return true if given an object with an id property', function() {
+      var source = { id: '3' };
+      var toFind = '3';
+      expect(compare.matchObjectId(source, toFind)).to.equal(true);
+    });
+
+    it('should return true if given an array of objects with an _id property', function() {
+      var source = [{ _id: '3' }];
+      var toFind = '3';
+      expect(compare.matchObjectId(source, toFind)).to.equal(true);
+    });
+
+    it('should return true if given an array of objects with an id property', function() {
+      var source = [{ id: '3' }];
+      var toFind = '3';
+      expect(compare.matchObjectId(source, toFind)).to.equal(true);
+    });
+
+    it('should not find a match in an array', function() {
+      var source = [
+        '0', '1', '2', '3', '4'
+      ];
+      var toFind = '5';
+      expect(compare.matchObjectId(source, toFind)).to.equal(false);
+    });
+
+    it('should not find a match in a string if different', function() {
+      var source = '3';
+      var toFind = '5';
+      expect(compare.matchObjectId(source, toFind)).to.equal(false);
+    });
+
+    it('should not return false if given an empty source array', function() {
+      var source = [];
+      var toFind = '5';
+      expect(compare.matchObjectId(source, toFind)).to.equal(false);
+    });
+
+    it('should throw an error if the haystack is undefined', function() {
+      var source = undefined;
+      var toFind = '5';
+      var fn = function() {
+        compare.matchObjectId(source, toFind);
+      };
+      expect(fn).to.throw(/Haystack undefined/i);
+    });
+
+    it('should throw an error if the needle is undefined', function() {
+      var source = [];
+      var toFind = undefined;
+      var fn = function() {
+        compare.matchObjectId(source, toFind);
+      };
+      expect(fn).to.throw(/Needle undefined/i);
+    });
+
+    it('should return false if it cant parse the haystack or needle', function() {
+      var source = 1;
+      var toFind = 1;
+      expect(compare.matchObjectId(source, toFind)).to.equal(false);
+    });
+  });
 });
